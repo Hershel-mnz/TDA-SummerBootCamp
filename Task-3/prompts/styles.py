8b9872ae4e0ae4cbea5a9103ac15b235cf6470e7
Hershel Shaun Menezes <hershelshaunmenezes@gmail.com>
2026-06-21 14:13:26 +0530

    Create styles.py

---
+++ Task-3/prompts/styles.py
@@ -0,0 +1,13 @@
+styles = {
+    "Anime":
+        "anime style, vibrant colors, highly detailed",
+
+    "Cyberpunk":
+        "cyberpunk style, neon lights, futuristic",
+
+    "Fantasy":
+        "fantasy art, magical atmosphere",
+
+    "Realistic":
+        "photorealistic, ultra detailed"
+}
